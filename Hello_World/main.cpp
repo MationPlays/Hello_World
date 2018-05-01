4b825ae1de26030a18502f5fcd9bc7428d47b14a
Gabriel Veiz <gabrielveiz@gabriels-macbook-air.local>
2018-05-01 19:37:41 +0200

    Initial Commit

---
+++ Hello_World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello_World
+//
+//  Created by Gabriel Veiz on 01.05.18.
+//  Copyright © 2018 Gabriel Veiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
